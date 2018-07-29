4cf9432069530d169a48d7fae41f316eddda6519
meancoin <41550660+meancoin@users.noreply.github.com>
2018-07-30 00:09:42 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
-<?php
+co<?php
 return array(
-        'api' => 'http://174.138.4.37/explorer',
+        'api' => 'http://meancoin.org/explorer',
         'blockTargetInterval' => 240,
         'coinUnits' => 100000000
 );
