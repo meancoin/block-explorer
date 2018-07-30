065fd46cd698c48bac176548ff858e7251c27cb3
meancoin <41550660+meancoin@users.noreply.github.com>
2018-07-30 13:26:46 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 co<?php
 return array(
-        'api' => 'http://meancoin.org/explorer',
+        'api' => 'http://explorer.meancoin.org/explorer',
         'blockTargetInterval' => 240,
         'coinUnits' => 100000000
 );
